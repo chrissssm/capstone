4aaafc37ececfd454ea5cec81b6c0812cbbbd413
Christopher M. <chrismandiratta@hotmail.com>
2024-05-04 14:47:09 +0200

    update

--- app.py
+++ app.py
@@ -1,6 +1,18 @@
+#### Overall setup 
+
 import streamlit as st
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pickle
 import plotly.express as px
+
+# Defining some general properties of the app
+st.set_page_config(
+    page_title= "Hotel Booking Optimizer",
+    page_icon = "🏨",
+    layout="wide"
+    )
 
 # Function to adjust capacity and calculate overbooked rooms
 def adjust_capacity(booking_data, max_capacity, show_rate):
